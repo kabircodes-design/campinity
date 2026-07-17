9348df442414155cbd4aaa057aa1bf643c098c77
Kabir <ofciamkabirsaiyed@gmail.com>
2026-07-17 19:39:26 +0530

    Move Firebase config to .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpaaoVFk_YhwCBnmigRF6yxoU6Zfmw4PU",
-    authDomain: "campinity-app.firebaseapp.com",
-    projectId: "campinity-app",
-    storageBucket: "campinity-app.firebasestorage.app",
-    messagingSenderId: "803486094288",
-    appId: "1:803486094288:web:202d12b1edf3b9aaa65fd5",
-};
-
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
+  
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
